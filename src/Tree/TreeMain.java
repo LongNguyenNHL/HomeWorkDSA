578dbb35da492601666bb67e48f195295b7ef310
LongNguyenNHL <nguyenhoanglongd239@gmail.com>
2025-06-17 00:52:34 +0930

    HomeWork Tree

--- src/Tree/TreeMain.java
+++ src/Tree/TreeMain.java
@@ -2,6 +2,7 @@ package Tree;
 
 public class TreeMain {
 	public static void main(String[] args) {
+		
 		Tree tree = new Tree();
 		
 		Node root = new Node();
@@ -17,28 +18,43 @@ public class TreeMain {
 		node.value = 40;
 		tree.root.children.add(node);
 		
-//		Node currentNode = tree.root;
-//		for (Node n : tree.root.children) {
-//			if (n.value == 30) {
-//				node = new Node();
-//				node.value = 20;
-//				n.children.add(node);
-//			} else if (n.value == 40) {
-//				node = new Node();
-//				node.value = 10;
-//				n.children.add(node);
-//			}
-//			
-//			if (n.value != 9) {
-//				currentNode = tree.root.children.get(0);
-//			}
-//			
-//		}
+		// add node 20 at node 30
+		node = new Node();
+		node.value = 20;
+		Node findNode = tree.findNodeDFS(30);
+		findNode.children.add(node);
 		
-		Node node20 = new Node();
-		node20.value = 20;
+		// add node 10 at node 30
+		node = new Node();
+		node.value = 10;
+		findNode = tree.findNodeDFS(30);
+		findNode.children.add(node);
 		
-		Node findNode = tree.findNode(30);
-		findNode.children.add(node20);
+		// add node 5 at node 10
+		node = new Node();
+		node.value = 5;
+		findNode = tree.findNodeDFS(10);
+		findNode.children.add(node);
+		
+		// add node 50 at node 40
+		node = new Node();
+		node.value = 50;
+		findNode = tree.findNodeDFS(40);
+		findNode.children.add(node);
+		
+		// add node 100 at node 1
+		node = new Node();
+		node.value = 100;
+		findNode = tree.findNodeBFS(1);
+		findNode.children.add(node);
+		
+		// Check
+		
+		findNode = tree.findNodeDFS(5);
+		System.out.println(findNode.value);
+		
+		findNode = tree.findNodeBFS(100);
+		System.out.println(findNode.value);
+
 	}
 }
